44c91f9cbb67ec27ac3cbeb32e7dcd27a6dfa3f6
Jaja Harris <jajaharris@gmail.com>
2019-12-14 11:02:38 -0700

    added login screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            children: <Widget>[
+              Image(image: AssetImage('assets/images/login_background.jpg'),)
+            ],
+          ),   
+        ),
+      ),      
+    );
+  }
+}
